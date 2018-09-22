1eebe6cb17f376f33c5a803497f958b1f2979fc8
maejinkim <sikhye823@gmail.com>
2018-09-22 18:07:55 +0900

    changeFragment add

--- MOHAGEE/app/src/main/java/com/example/maedin/mohagee/activity/MainActivity.java
+++ MOHAGEE/app/src/main/java/com/example/maedin/mohagee/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.maedin.mohagee.activity;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -86,6 +87,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawerLayout.closeDrawer(GravityCompat.START);//Drawer를 닫음
         return true;
+    }
+
+
+    public void ChangeFragment(Fragment fragment)
+    {
+        FragmentManager manager = getSupportFragmentManager();
+        manager.beginTransaction().replace(R.id.content_main, fragment).commit();
     }
 
 
